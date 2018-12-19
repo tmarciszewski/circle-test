1ec5fdb503ebb92802113bf8a34002a97419f2f3
Tomasz Marciszewski <tmarciszewski@brightbytes.net>
2018-12-19 19:26:41 +0100

    Prettier

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-'use strict';
+"use strict";
 
-const app = require('./app');
+const app = require("./app");
 const port = process.env.port || 3000;
-app.listen(port, () => console.log(`Test App listening on port ${port}!`))
+app.listen(port, () => console.log(`Test App listening on port ${port}!`));
--- test.js
+++ test.js
@@ -1,49 +1,53 @@
-'use strict';
+"use strict";
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 const expect = chai.expect;
-const request = require('supertest');
-const app = require('./app');
+const request = require("supertest");
+const app = require("./app");
 
 chai.use(chaiHttp);
 
-describe('Test API', () => {
-  it('should pass', () => {
+describe("Test API", () => {
+  it("should pass", () => {
     expect(true).to.equal(true);
   });
 
-  describe('index', () => {
-    it('should return status', async () => {
+  describe("index", () => {
+    it("should return status", async () => {
       const { body } = await request(app)
-        .get('/')
-        .expect('Content-Type', /json/)
+        .get("/")
+        .expect("Content-Type", /json/)
         .expect(200);
 
-      expect(body).to.have.property('status', 'operational');
-    })
-  });
-
-  describe('random', () => {
-    it('should return number', async () => {
-      const { body } = await request(app)
-        .get('/random')
-        .expect('Content-Type', /json/)
-        .expect(200);
-
-      expect(body).to.have.property('number').which.is.a('number');
+      expect(body).to.have.property("status", "operational");
     });
   });
 
-  describe('version', () => {
-    it('should return version', async () => {
+  describe("random", () => {
+    it("should return number", async () => {
       const { body } = await request(app)
-        .get('/version')
-        .expect('Content-Type', /json/)
+        .get("/random")
+        .expect("Content-Type", /json/)
         .expect(200);
 
-        expect(body).to.have.property('version').which.is.a('string');
-        expect(body).to.have.property('name');
+      expect(body)
+        .to.have.property("number")
+        .which.is.a("number");
+    });
+  });
+
+  describe("version", () => {
+    it("should return version", async () => {
+      const { body } = await request(app)
+        .get("/version")
+        .expect("Content-Type", /json/)
+        .expect(200);
+
+      expect(body)
+        .to.have.property("version")
+        .which.is.a("string");
+      expect(body).to.have.property("name");
     });
   });
 });
